3e33a84d9d93e671897c547d1d981c7c2264e3b4
SicilyXiao <86068048+SicilyXiao@users.noreply.github.com>
2022-02-11 19:39:15 -0500

    Add files via upload

---
+++ Tipster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Tipster
+//
+//  Created by Yixiao Xiao on 2/11/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
---
+++ Tipster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by Yixiao Xiao on 2/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
